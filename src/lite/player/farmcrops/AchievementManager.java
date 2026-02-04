15b8ed6efbdfdbbf9b2e804471c0c6169d6543e2
SamielXD2 <samiellxd@gmail.com>
2026-02-04 10:11:16 +0800

    Add files via upload

---
+++ src/lite/player/farmcrops/AchievementManager.java
@@ -0,0 +1,29 @@
+package player.farmcrops;
+
+import org.bukkit.entity.Player;
+import java.util.*;
+
+/**
+ * LITE EDITION STUB - This class does nothing
+ * Upgrade to Premium for full functionality
+ */
+public class AchievementManager {
+    
+    public AchievementManager(FarmCrops plugin) {
+        // Lite edition - no functionality
+    }
+    
+    public void checkAchievements(Player player) {
+        // Lite edition - no functionality
+    }
+    
+    public Collection<AchievementData> getAllAchievements() {
+        return Collections.emptyList();
+    }
+    
+    public static class AchievementData {
+        public String id;
+        public String name;
+        public String description;
+    }
+}
---
+++ src/lite/player/farmcrops/CollectionManager.java
@@ -0,0 +1,18 @@
+package player.farmcrops;
+
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+
+/**
+ * LITE EDITION STUB
+ */
+public class CollectionManager {
+    
+    public CollectionManager(FarmCrops plugin) {
+        // Lite edition - no functionality
+    }
+    
+    public void addCropToCollection(Player player, Material cropType) {
+        // Lite edition - no functionality
+    }
+}
